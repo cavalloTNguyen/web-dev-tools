52b2d03a95734f596bd13dd6922f298c17d29615
Thanh Nguyen <thanh.nguyen@cavallo.com>
2026-03-02 15:56:31 -0500

    fix: remove base name

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,19 +3,16 @@ import { App } from "./App";
 import { ZlibDecoder } from "./screens/ZlibDecoder/ZlibDecoder";
 import { PercentChange } from "./screens/PercentChange/PercentChange";
 
-const router = createHashRouter(
-  [
-    {
-      path: "/",
-      Component: App,
-      children: [
-        { path: "/", element: <div>all the fun is in the menu</div> },
-        { path: "zlib-decoder", Component: ZlibDecoder },
-        { path: "percent-change", Component: PercentChange },
-      ],
-    },
-  ],
-  { basename: import.meta.env.BASE_URL },
-);
+const router = createHashRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      { path: "/", element: <div>all the fun is in the menu</div> },
+      { path: "zlib-decoder", Component: ZlibDecoder },
+      { path: "percent-change", Component: PercentChange },
+    ],
+  },
+]);
 
 export default router;
