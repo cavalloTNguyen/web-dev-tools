4a440e46918698498d583268f8bd3f0f6a0b8112
Thanh Nguyen <thanh.nguyen@cavallo.com>
2026-03-02 16:03:37 -0500

    fix: remove trailing slash

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/web-dev-tools/",
+  base: "/web-dev-tools",
   plugins: [react()],
   server: {
     port: 7880,
